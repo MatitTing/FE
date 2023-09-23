2ecc64fa15656e97c5e6785fd39ce1ec002b5311
소연 <27xo.yeon@gmail.com>
2023-09-23 21:01:02 +0900

    카카오 맵 추가 및 식당 검색 기능 구현

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+/* eslint-disable @next/next/no-sync-scripts */
+import {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+import Script from "next/script";
 
 const Document = () => {
   return (
@@ -11,6 +18,10 @@ const Document = () => {
       <body>
         <Main />
         <NextScript />
+        <Script
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_KEY}&libraries=services,clusterer&autoload=false`}
+          strategy="beforeInteractive"
+        />
       </body>
     </Html>
   );
