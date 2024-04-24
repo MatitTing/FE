d8a9bba8e14a55b340fbb7fd222ec61a30b9c34b
minchodang <minsu910725@gmail.com>
2024-04-24 16:21:19 +0900

    Fix: 마이페이지 및 파티 현황 조회 api 타입 수정 및 매너 온도 계산 로직 추가

--- src/api/getPartyJoin.ts
+++ src/api/getPartyJoin.ts
@@ -1,20 +1,22 @@
-import variableAssignMent from "@utils/variableAssignment";
-import defaultRequest from "src/lib/axios/defaultRequest";
-import { PartyJoinResponse } from "types/party/join/PartyJoinResponse";
+import variableAssignMent from '@utils/variableAssignment';
+import defaultRequest from 'src/lib/axios/defaultRequest';
+import { PartyJoinResponse } from 'types/party/join/PartyJoinResponse';
 
+export type GetPartyJoinRequestRole = 'HOST' | 'VOLUNTEER';
 interface getPartyJoinParameter {
-  role: string;
+    page: number;
+    size: number;
+    sort: string;
+    role: GetPartyJoinRequestRole;
 }
 
-export const API_GET_PARTY_JOIN_KEY = "/api/party/party-join?role={{role}}";
+export const API_GET_PARTY_JOIN_KEY = '/api/party/party-join?role={{role}}';
 
-const getPartyJoin = async ({
-  role,
-}: getPartyJoinParameter): Promise<PartyJoinResponse[]> => {
-  const { data } = await defaultRequest.get(
-    variableAssignMent(API_GET_PARTY_JOIN_KEY, { role: role })
-  );
-  return data;
+const getPartyJoin = async ({ role }: getPartyJoinParameter): Promise<PartyJoinResponse[]> => {
+    const { data } = await defaultRequest.get(
+        variableAssignMent(API_GET_PARTY_JOIN_KEY, { role: role }),
+    );
+    return data;
 };
 
 export default getPartyJoin;
